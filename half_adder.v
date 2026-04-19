0481b30bd762e06b6a31a97112ec77ccb4d6f065
Aditya Ugare <adityaugare13@gmail.com>
2026-04-19 13:52:54 +0530

    Implement full adder using two half adders

---
+++ half_adder.v
@@ -0,0 +1,13 @@
+
+
+module full_adder(
+    input a,b,cin,
+    output sum,carry
+    );
+    
+    wire w1,w2,w3;
+    half_adder H1(.a(a),.b(b),.sum(w1),.carry(w2));
+    half_adder H2(.a(w1),.b(cin),.sum(sum),.carry(w3));
+    or OR1(carry,w2,w3)
+    
+endmodule
