50e1dec782e8609f4fd7a87ee27da215562e8b67
Aditya Ugare <adityaugare13@gmail.com>
2026-04-19 14:05:25 +0530

    Refactor full_adder module syntax and formatting

--- full_adder.v
+++ full_adder.v
@@ -1,12 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.04.2026 13:16:08
+// Design Name: 
+// Module Name: full_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module full_adder(
-    input a,b,cin,
-    output sum,carry
-    );
-    
-    wire w1,w2,w3;
-    half_adder H1(.a(a),.b(b),.sum(w1),.carry(w2));
-    half_adder H2(.a(w1),.b(cin),.sum(sum),.carry(w3));
-    or OR1(carry,w2,w3)
-    
+
+module full_adder(input a,b,cin,output sum,carry);
+wire w1,w2,w3;
+half_adder H1(.a(a),.b(b),.sum(w1),.carry(w2));
+half_adder H2(.a(w1),.b(cin),.sum(sum),.carry(w3));
+or OR1(carry,w2,w3);
 endmodule
