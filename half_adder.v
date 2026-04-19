372683147e9502839bf03755d757e981c2e54696
Aditya Ugare <adityaugare13@gmail.com>
2026-04-19 13:53:50 +0530

    Refactor full_adder to half_adder module

--- half_adder.v
+++ half_adder.v
@@ -1,13 +1,9 @@
 
-
-module full_adder(
-    input a,b,cin,
+module half_adder(
+    input a, b,
     output sum,carry
     );
     
-    wire w1,w2,w3;
-    half_adder H1(.a(a),.b(b),.sum(w1),.carry(w2));
-    half_adder H2(.a(w1),.b(cin),.sum(sum),.carry(w3));
-    or OR1(carry,w2,w3)
-    
+    xor HA1(sum,a,b));
+    and HA2(carry,a,b);
 endmodule
